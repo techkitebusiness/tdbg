300c75a1ba856581c55f354b2aff94960be1116d
basu <basu@basu.com>
2024-05-04 02:51:51 -0700

    require('dotenv') add in config

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2/promise');
-
+require('dotenv').config();
 const connection = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
